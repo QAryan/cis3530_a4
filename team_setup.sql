b3400edd6c8d8cd545381a37ab47eef740d49197
ngraha11 <ngraha11@uoguelph.ca>
2025-11-26 16:59:03 -0500

    added the two indexes to setup sql file and added justification to the README

--- team_setup.sql
+++ team_setup.sql
@@ -3,3 +3,5 @@ CREATE TABLE IF NOT EXISTS app_user (
     username TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL
 );
+CREATE INDEX IF NOT EXISTS idx_workson_pno ON Works_On (Pno);
+CREATE INDEX IF NOT EXISTS idx_employee_ssn ON Employee (Ssn);
